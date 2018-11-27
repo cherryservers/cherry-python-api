b7614f3e36e046a1883d2623f1fc1dfe08928ebb
Arturas Razinskij <arturas@amac.local>
2018-11-27 11:05:53 +0200

    Fixes

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='cherry',
       version='0.1',
       description='Cherry Servers API client',
-      url='http://github.com/arturasr/cherry-python-api',
+      url='https://bitbucket.org/cherryservers/cherry-python-api',
       author='Aarturas Razinskij',
       author_email='arturas.razinskij@cherryservers.com',
       license='LGPL v3',
       packages=['cherry'],
+      install_requires='requests',
       zip_safe=False)
